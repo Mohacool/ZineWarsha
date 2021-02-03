eb7c02849e5bcf985d937b8847b90aaa6d30ef88
Mohasalama <msalama2007@hotmail.com>
2021-02-03 13:24:17 -0500

    change audio

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 // const cursor = curDot()
 
+const small_cursor_diameter = 15;
+const grow_scale = 4;
+const big_cursor_diameter = small_cursor_diameter*grow_scale;
+
 // or, set as you want
 const cursor = curDot({
   zIndex: 2,
-  diameter: 15,
+  diameter: small_cursor_diameter,
   borderWidth: 1,
   borderColor: '#be1f1f',
   easing: 4,
@@ -13,10 +17,31 @@ const cursor = curDot({
 
 cursor.over('.ornament', {
 	background: '#be1f1f',
-	broderWidth: 2,
-	scale: 10,
+  broderWidth: 2,
+  easing: 40,
+	scale: grow_scale,
   })
 
+  $('.ornament').on('click',function(event){
+    // alert(event.clientX+", "+event.clientY);
+    let mouseX = event.clientX - big_cursor_diameter/2;
+    let mouseY = event.clientY - big_cursor_diameter/2;
+    $('.circle').css('display','block');
+    $('.circle').css('transform',`translate(${mouseX}px,${mouseY}px) scale(1)`);
+    var t5 = gsap.timeline();
+    t5.to('.circle',{transform:`translate(${mouseX}px,${mouseY}px) scale(50)`,duration:5});
+    
+    $('.circle').animate({
+      'transform':`translate(${mouseX}px,${mouseY}px) scale(4)`
+    },1000);
+
+  })
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// ctx.beginPath();
+// ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+// ctx.stroke();
  
 
 
